758442acd42e2fbea77c0e02687ad91e6e6f6c12
Ihor Dmytriv <dmytriwihor@gmail.com>
2024-04-13 09:33:57 +0200

    Wrote models and made migrations

--- taxi/models.py
+++ taxi/models.py
@@ -1,3 +1,18 @@
+from django.conf import settings
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class Manufacturer(models.Model):
+    name = models.CharField(max_length=60, unique=True)
+    country = models.CharField(max_length=60)
+
+
+class Car(models.Model):
+    model = models.CharField(max_length=60)
+    manufacturer = models.ForeignKey(Manufacturer, on_delete=models.CASCADE, related_name='cars')
+    drivers = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='cars')
+
+
+class Driver(AbstractUser):
+    license_number = models.CharField(max_length=60, unique=True)
