8b3496287ba28fda8b9160bcfec8cd61d1e05fbf
Ihor Dmytriv <dmytriwihor@gmail.com>
2024-04-13 16:34:47 +0200

    Solution

--- taxi/admin.py
+++ taxi/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from taxi.models import Manufacturer, Car, Driver
+
+
+@admin.register(Driver)
+class DriverAdmin(UserAdmin):
+    fieldsets = UserAdmin.fieldsets + (("Additional info", {"fields": ("license_number", )}), )
+    add_fieldsets = UserAdmin.add_fieldsets + (("Additional info", {"fields": ("license_number", )}), )
+    list_display = UserAdmin.list_display + ("license_number", )
+
+
+@admin.register(Car)
+class CarAdmin(admin.ModelAdmin):
+    search_fields = ("model", )
+    list_filter = ("manufacturer__name", "manufacturer__country")
+
+
+admin.site.register(Manufacturer)
--- taxi/models.py
+++ taxi/models.py
@@ -7,11 +7,17 @@ class Manufacturer(models.Model):
     name = models.CharField(max_length=60, unique=True)
     country = models.CharField(max_length=60)
 
+    def __str__(self):
+        return f"{self.name} ({self.country})"
+
 
 class Car(models.Model):
     model = models.CharField(max_length=60)
     manufacturer = models.ForeignKey(Manufacturer, on_delete=models.CASCADE, related_name='cars')
     drivers = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='cars')
+
+    def __str__(self):
+        return f"{self.model}, ({self.manufacturer})"
 
 
 class Driver(AbstractUser):
